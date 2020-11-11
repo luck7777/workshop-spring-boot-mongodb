f7fd35bc840e8c9f1139ebc5fa63923f1a3d80d7
Lucas Araujo <ljca84@gmail.com>
2020-11-11 14:33:14 -0300

    Atualização de usuário com PUT

--- src/main/java/com/luck7777/workshopmongo/resources/UserRecource.java
+++ src/main/java/com/luck7777/workshopmongo/resources/UserRecource.java
@@ -45,9 +45,18 @@ public class UserRecource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id){
+		User obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 }

824e105c3bcf77979d5108b20d9a3234cf06a39e
Lucas Araujo <ljca84@gmail.com>
2020-11-12 06:50:16 -0300

    Obtendo um post por id

---
+++ src/main/java/com/luck7777/workshopmongo/resources/PostRecource.java
@@ -0,0 +1,25 @@
+package com.luck7777.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luck7777.workshopmongo.domain.Post;
+import com.luck7777.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostRecource {
+	
+	@Autowired
+	private PostService service;	
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post obj = service.findById(id);		
+		return ResponseEntity.ok().body(obj);
+	}	
+}
---
+++ src/main/java/com/luck7777/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.luck7777.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luck7777.workshopmongo.domain.Post;
+import com.luck7777.workshopmongo.repository.PostRepository;
+import com.luck7777.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));		
+	}	
+}
